541d8006d71026a20f616b06f2b1d45ce4f23dfb
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-14 11:59:43 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Qanoar: ['Qanoar', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
